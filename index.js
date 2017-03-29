5a42a805e72c4d154e91c9dae2374ca96da4a27f
Fabricio Catae <fcatae@microsoft.com>
2017-03-29 20:58:59 -0300

    organized code

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // In renderer process (web page).
 const {ipcRenderer} = require('electron')
-console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
 
-ipcRenderer.on('asynchronous-reply', (event, arg) => {
-  console.log(arg) // prints "pong"
-})
+// console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
 
-ipcRenderer.send('asynchronous-message', 'ping')
+// ipcRenderer.on('asynchronous-reply', (event, arg) => {
+//   console.log(arg) // prints "pong"
+// })
+
+// ipcRenderer.send('asynchronous-message', 'ping')
 
 function newOrganizerWindow() {
     ipcRenderer.send('organizer.new', 'orgNEW');
--- main.js
+++ main.js
@@ -11,33 +11,14 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 240, height: 180, frame: false})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+function startWindow () {
+  mainWindow = createWindow();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', startWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -81,24 +62,31 @@ const {ipcMain} = require('electron')
 
 ipcMain.on('organizer.new', (event, arg) => {
 
-    console.log('Creating new window')  // prints "ping"
-    event.returnValue = 'pong'
-
     let mainWindow = createWindow();
 
-    function createWindow() {
-      let win = new BrowserWindow({width: 240, height: 180, frame: false})
-
-      win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-      }));
-
-      win.on('closed', function () {
-        win = null
-      });
-    }
+});
 
 
-})
+function createWindow() {
+
+    console.log('Creating new window')  // prints "ping"
+
+    let win = new BrowserWindow({width: 240, height: 180, frame: false, show: false,
+    skipTaskbar: true})
+
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+
+    win.once('ready-to-show', () => {
+      win.show()
+    })
+
+    win.on('closed', function () {
+      win = null
+    });
+
+    return win;
+}
